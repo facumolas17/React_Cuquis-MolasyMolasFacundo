66236c44c1c83444c6a79bf0913c3700f537adfe
Facu Molas <facundomolasymolas@gmail.com>
2022-09-20 17:31:52 +0200

    agregué un subtitulo a la card y modifiqué el css

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ItemCount from './ItemCount';
-const Item = ({name, imagenUrl,precio}) => {
+const Item = ({name, title, imagenUrl,precio}) => {
     const onAdd= (cont) =>{
 
         console.log(`Se agregaron al carrito ${cont} unidades`);
@@ -11,6 +11,7 @@ const Item = ({name, imagenUrl,precio}) => {
           <div className="card-body text-center">
               <img className='img-fluid' src={require(`../assets/images/img-${imagenUrl}.jpg`)} alt='imagen producto' />
               <h4 className='card-title pt-2'>#{name}</h4>
+              <h5 className='card-subtitle text-muted' >{title}</h5>
               <h6 className='card-text pt-3 text-secondary'>${precio}</h6>
               <ItemCount initial={1} stock={5} onAdd={onAdd}/>
               
--- src/contenedores/productos.jsx
+++ src/contenedores/productos.jsx
@@ -6,7 +6,7 @@ const productos=[
         "precio":15700,
         "name":"CAROL",
         "image": "carol",
-        "title": "CAROL mesa de juegos",
+        "title": "mesa de juegos",
         "descriptionGeneral":"Mesa de madera. Niños 1 a 8 años. Tapa en madera melamina color blanco espesor 18mm. Patas laterales panel fenólico tono natural espesor 14 mm.Dimensiones: 60 cm profundidad x 52 cm ancho x 45 cm alto."
   },
   {
@@ -15,7 +15,7 @@ const productos=[
         "precio":8900,
         "name":"ELEONOR",
         "image": "eleonor",
-        "title": "ELEONOR Banquito escalera",
+        "title": "banquito escalera",
         "descriptionGeneral":"Banquito escalera de madera. Niños 1 a 6 años. Taburete y escalón en madera melamina color blanco espesor 18mm. Laterales panel fenólico tono natural espesor 14 mm. Dimensiones: 38 cm profundidad x 30 cm ancho x 20 cm alto"
   },
   {     "id":3,  
@@ -23,7 +23,7 @@ const productos=[
         "precio":10900,
         "name":"GALILEO",
         "image": "galileo",
-        "title": "GALILEO banquito de juegos",
+        "title": "banquito de juegos",
         "descriptionGeneral":"Banquito de madera. Niños 1 a 8 años. Taburete en madera melamina color blanco espesor 18mm. Laterales panel fenólico tono natural espesor 14 mm. Dimensiones: 38 cm profundidad x 32 cm ancho x 29 cm alto."
     
   },
@@ -33,7 +33,7 @@ const productos=[
         "precio":12500,
         "name":"PEPPER",
         "image": "pepper",
-        "title": "PEPPER silloncito de juegos",
+        "title": "silloncito de juegos",
         "descriptionGeneral":"Silloncito de madera. Niños 1 a 8 años. Taburete y respaldo en madera melamina color blanco espesor 18mm. Laterales panel fenólico tono natural espesor 14 mm. Dimensiones: 38 cm profundidad x 32 cm ancho x 40 cm alto."
 
   },
@@ -43,7 +43,7 @@ const productos=[
         "precio":25700, 
         "name":"RINGO",
         "image": "ringo",
-        "title": "RINGO baúl de guardado",
+        "title": "baúl de guardado",
         "descriptionGeneral":"Baúl de madera. Niños 1 a 8 años.Taburete y espacio de guardado en madera melamina color blanco espesor 18mm. Laterales panel fenólico tono natural espesor 14 mm. Herrajes en acero inoxidable. Bisagras soft close. Manijas de agarre. Dimensiones: 70 cm largo x 45 cm ancho x 38 cm alto."
   },
 
@@ -53,7 +53,7 @@ const productos=[
         "precio":12500,  
         "name":"ROBIN",
         "image": "robin",
-        "title": "ROBIN Banquito baúl de juegos",
+        "title": "Banquito baúl de juegos",
         "descriptionGeneral":"Banquito baúl de madera. Niños 1 a 8 años. Taburete y espacio de guardado en madera melamina color blanco espesor 18mm. Laterales panel fenólico tono natural espesor 14 mm. Herrajes en acero inoxidable. Dimensiones: 38 cm profundidad x 32 cm ancho x 29 cm alto."
 }
 
