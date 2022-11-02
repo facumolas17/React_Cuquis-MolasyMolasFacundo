be70664281d077bb62d2d00fe2e2ef4179dfdf62
Facu Molas <facundomolasymolas@gmail.com>
2022-11-02 12:50:03 -0300

    Acomodé el código del componente checkout

--- src/componentes/Checkout.jsx
+++ src/componentes/Checkout.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Checkout = () => {
-
-  const {nroOrden} = useParams();
+  const { nroOrden } = useParams();
   return (
     <div>
-      <div className='text-center'>
+      <div className="text-center">
         <h3>¡Tu compra fue realizada con éxito!</h3>
-        <h3>El localizador de tu compra es: <strong className='text-warning'>{nroOrden}</strong></h3>
+        <h3>
+          El localizador de tu compra es:{" "}
+          <strong className="text-warning">{nroOrden}</strong>
+        </h3>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Checkout;
